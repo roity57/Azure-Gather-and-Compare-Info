cd4ebc592fc51e28a7b3294ef1cb1a75817e88be
Dave <roity57@gmail.com>
2021-10-02 18:08:20 +1000

    Update AzGatherInfo.ps1
    Added Get-AzApiManagement to array

--- AzGatherInfo.ps1
+++ AzGatherInfo.ps1
@@ -2,6 +2,7 @@
 # Gather Azure information via Az Powershell "Get-Az" commands v2.0 - 27/6/2020
 # v2.1 - 12/7/2020 - Added call to new function to enumerate Virtual Network Gateways and Express Route Circuits and gather info, comment out if undesired.
 # v2.2 - 19/9/2021 - Added call to new function to fetch DNS zones and record details, comment out if undesired. Also added Application Gateway Get command to array.
+# v2.3 - 2/10/2021 - Added Get-AzApiManagement to the array
 # Tested in PowerShells 5.x & 7.x environments with Az Module 4.x & 6.x on a Windows VM
 # Utilised two other modules which each contain one function
 
@@ -9,7 +10,9 @@
 
 
 #Define the commands to run
-[array]$azget="Get-AzResource","Get-AzNetworkSecurityGroup","Get-AzVirtualNetwork","Get-AzRouteTable","Get-AzPublicIpAddress","Get-AzNetworkInterface","Get-AzLoadBalancer","Get-AzNetworkWatcher","Get-AzRouteFilter","Get-AzApplicationGateway"
+[array]$azget="Get-AzResource","Get-AzNetworkSecurityGroup","Get-AzVirtualNetwork","Get-AzRouteTable","Get-AzPublicIpAddress","Get-AzNetworkInterface","Get-AzLoadBalancer","Get-AzNetworkWatcher","Get-AzRouteFilter","Get-AzApplicationGateway","Get-AzApiManagement"
+
+
 
 #Enumerate the Azure Contexts available to loop through
 [array]$aztenant=Get-AzContext -ListAvailable 
